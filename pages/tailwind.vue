c293369774da87c8cbb9ed5c11d2de3ca23dbc2b
finn2212 <stolle.finn@googlemail.com>
2023-11-25 08:46:09 +0200

    removed post css

--- pages/tailwind.vue
+++ pages/tailwind.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import VHeader from '@/components/layout/VHeader.vue';
-
+import VForm from '~/components/layout/VFORM.vue';
+import VPrices from '~/components/layout/VPrices.vue';
+import VAbout from '~/components/layout/VAbout.vue';
 
 useBreadcrumbs([
   {
@@ -18,5 +20,9 @@ export default {
 
 <template>
   <LayoutBreadcrumbs />
- <VHeader />
+  <VHeader />
+  <VForm />
+  <VPrices />
+  <VAbout />
+
 </template>
