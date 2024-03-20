a7c4f31685cbf07879ad30cfbf7996454f02b1a2
finn2212 <stolle.finn@googlemail.com>
2024-03-20 14:35:54 +0800

    coockie bot

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,16 @@ export default defineNuxtConfig({
           href: "https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap",
         },
       ],
+      script: [
+        {
+          type: "text/javascript",
+          ab: "1",
+          src: "https://cdn.consentmanager.net/delivery/autoblocking/64f17ad2414dc.js",
+          host: "d.delivery.consentmanager.net",
+          cdn: "cdn.consentmanager.net",
+          codesrc: "0",
+        },
+      ],
     },
   },
   routeRules: {
