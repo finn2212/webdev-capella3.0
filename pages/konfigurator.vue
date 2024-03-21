dbdebd7039d720299ada69b2ad2fc5a6f16702e7
f-stolle-ext-tqgg <finn.stolle.external@tqgg.de>
2024-03-21 15:27:32 +0800

    loading added

--- pages/konfigurator.vue
+++ pages/konfigurator.vue
@@ -68,6 +68,8 @@ const weight = ref(0);
 const swEndPoint = ref("");
 const accesstoken = ref("");
 const isLoading = ref(false);
+const isLoadingPrice = ref(false);
+
 const voices = ref<Voice[]>([]); // Array of 'Voice'
 const newVoice = ref({
   name: '',
@@ -88,6 +90,7 @@ const formValidations = reactive({
 
 
 onMounted(async () => {
+  isLoadingPrice.value = true
   updateCurrentDiscountId(discount.value); // Make sure calculatePrice is defined appropriately
   sessionContextData.value = await getSessionContext(apiInstance);
   const metaInfo = sessionContextData.value.salesChannel.customFields;
@@ -96,6 +99,7 @@ onMounted(async () => {
   pagePriceHigh.value = metaInfo.custom_meta_pagePriceHigh || 0;
   baseVoicePrice.value = metaInfo.custom_meta_voice_handling || 0;
   voicePagePrice.value = metaInfo.custom_meta_baseVoicePrice || 0;
+  isLoadingPrice.value = false
   calculatePrice();
 });
 
@@ -145,12 +149,7 @@ const createProduct = async (accessToken: string, productData: any) => {
     }
   }
 };
-const preCheck = () => {
-  createAndFetch();
-  // if (validate()) {
-  //   createAndFetch();
-  // }
-};
+
 const validate = () => {
   if (projectType.value === 1) {
     if (pdf1.value === "" || productName.value === "") {
@@ -734,7 +733,7 @@ export default {
       </div>
     </section>
     <section>
-      <form @submit.prevent="preCheck" class="mt-3">
+      <form @submit.prevent="createAndFetch" class="mt-3">
         <div class="flex flex-col md:flex-row">
           <div class="grey md:w-6/9 w-full border-12 border-white flex flex-col p-4">
             <div class="flex flex-wrap pt-3">
@@ -1260,10 +1259,22 @@ export default {
                 </div>
               </div>
               <div>
-                <div class="pt-3">
+                <div class="pt-3" v-if="!isLoadingPrice">
                   <p>Ihr Preis für die gewünschte Auflagenhöhe:</p>
                   <h2 class="font-bold pt-3">€ {{ projectPriceString }}</h2>
                   <p style="font-size: x-small;">Preis inkl. 7% MwSt., ggf. zzgl. Versandkosten</p>
+                </div>
+                <div v-else role="status">
+                  <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor" />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill" />
+                  </svg>
+                  <span class="sr-only">Loading...</span>
                 </div>
               </div>
               <div>
